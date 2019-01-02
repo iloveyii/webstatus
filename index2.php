12f0d152f7fa61cdea6a704bd24f1fc1072f1dde
Hazrat Ali <iloveyii@yahoo.com>
2019-01-02 16:14:43 +0100

    Added user input validation

--- index2.php
+++ index2.php
@@ -4,25 +4,29 @@ require_once "config/app.php";
 require_once "vendor/autoload.php";
 
 use App\Models\Database;
+use App\Models\Log;
 use App\Models\Webstatus;
 use Rhumsaa\Uuid\Uuid;
-use App\Models\Log;
 
 
-if ( isset($_GET['debug']) && $_GET['debug'] == '1') {
+// User input validation
+$debug = (isset($_GET['debug']) && is_numeric($_GET['debug'])) ? (int)$_GET['debug'] : null;
+
+if ($debug === 1) {
     phpinfo();
     die();
-} elseif ( isset($_GET['debug']) && $_GET['debug'] == '2') {
+} elseif ($debug === 2) {
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
 }
 
-if (isset($_GET['site'])) {
-    $sql = "SELECT * FROM websites WHERE id = ${_GET['site']}";
+if (isset($_GET['site']) && is_numeric($_GET['site'])) {
+    $sql = sprintf("SELECT * FROM websites WHERE id = %d", (int)$_GET['site']);
 } else {
     $sql = "SELECT * FROM websites WHERE BENCHMARK(100000000, 10*10) OR 1=1";
 }
 
+// Connect to database and fetch rows
 $rows = Database::connect()->selectAll($sql);
 
 foreach ($rows as $row) {
