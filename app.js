d935841ad390c7ab36dd2ed85886144f08d17612
Hazrat Ali <iloveyii@yahoo.com>
2019-01-03 07:47:58 +0100

    Fix ajax

--- app.js
+++ app.js
@@ -5,20 +5,25 @@ new Vue({
         users: {},
         urls: []
     },
+
     created: function () {
         var api = this.$http;
         var that = this;
 
+        this.$http.get('https://jsonplaceholder.typicode.com/users')
+            .then(function (response) {
+                this.users = response.data;
+                this.users = this.users.map(function (user) {
+                    user.status = Math.round(Math.random());
+                    return user;
+                });
+                console.log(this.users);
+            });
         this.$http.get('http://localhost:8888/index.php?format=json')
             .then(function (response) {
                 this.urls = response.data;
                 this.urls = this.urls.map(function (url) {
                     url.status = Math.round(Math.random()) + Math.round(Math.random());
-
-
-
-
-
                     return url;
                 });
 
@@ -30,6 +35,7 @@ new Vue({
                                 var key = Object.keys(response.data);
                                 var value = response.data[key];
                                 this.urls[index].status = value === true ? 1 : 0;
+                                that.$forceUpdate();
                                 that.$forceUpdate();
                             }
                             console.log('Inside for each after fetch: ', url);
