9ff4289fd26d3b25f578d886992339819b41552d
Hazrat Ali <iloveyii@yahoo.com>
2019-01-03 08:40:37 +0100

    Fixed urls

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ new Vue({
     created: function () {
         var api = this.$http;
 
-        this.$http.get('http://localhost:8888/index.php?format=json')
+        this.$http.get('/index.php?format=json')
             .then(function (response) {
                 this.urls = response.data;
                 this.urls = this.urls.map(function (url, index) {
                     url.status = 2;
-                    api.get('http://localhost:8888/index.php?format=json&debug=3&site=' + url.id)
+                    api.get('/index.php?format=json&debug=3&site=' + url.id)
                         .then(function (response) {
                             if (response.data) {
                                 var key = Object.keys(response.data);
